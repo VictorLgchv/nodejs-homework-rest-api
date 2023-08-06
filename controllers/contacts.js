c2937729b2d0a382ccbc3a3b6f9e17d25a5272b1
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-08-06 13:25:18 +0300

    added controllers

---
+++ controllers/contacts.js
@@ -0,0 +1,66 @@
+const { HttpError, ctrlWrapper } = require("../helpers");
+const { Contact } = require("../models/contact");
+
+const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt");
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const result = await Contact.findOne({ _id: req.params.contactId });
+  if (!result) {
+    throw new HttpError(404);
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const { _id: owner } = req.user;
+
+  const result = await Contact.create({ ...req.body, owner });
+  res.status(201).json(result);
+};
+
+const deletById = async (req, res) => {
+  const result = await Contact.findByIdAndRemove(req.params.contactId);
+  if (!result) {
+    throw new HttpError(404);
+  }
+  res.json({ message: "Contact deleted" });
+};
+
+const updateById = async (req, res) => {
+  const result = await Contact.findByIdAndUpdate(
+    req.params.contactId,
+    req.body,
+    { new: true }
+  );
+  if (!result) {
+    throw new HttpError(404);
+  }
+
+  res.json(result);
+};
+
+const updateFavorite = async (req, res) => {
+  const result = await Contact.findByIdAndUpdate(
+    req.params.contactId,
+    req.body,
+    { new: true }
+  );
+  if (!result) {
+    throw new HttpError(404);
+  }
+
+  res.json(result);
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  addContact: ctrlWrapper(addContact),
+  deletById: ctrlWrapper(deletById),
+  updateById: ctrlWrapper(updateById),
+  updateFavorite: ctrlWrapper(updateFavorite),
+};
