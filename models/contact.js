c67894d9c736849a2448f70da7b750c08a8f82e2
VictorLgchv <108681513+VictorLgchv@users.noreply.github.com>
2023-08-06 13:25:39 +0300

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,28 +1,59 @@
 const { Schema, model } = require("mongoose");
+const Joi = require("joi");
 
-const { handleMongooseError } = require("../helpers/index");
+const { handleMongooseError } = require("../helpers");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const phoneRegexp = /^\(\d{3}\) \d{3}-\d{4}$/;
+const emailRegexp =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: true,
+    },
+    phone: {
+      type: String,
+      match: phoneRegexp,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 contactSchema.post("save", handleMongooseError);
 
+const joiSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  phone: Joi.string().regex(phoneRegexp).required(),
+  favorite: Joi.boolean(),
+});
+
+const joiSchemaFavorite = Joi.object({
+  favorite: Joi.boolean(),
+});
+
+const schemas = {
+  joiSchema,
+  joiSchemaFavorite,
+};
+
 const Contact = model("contact", contactSchema);
 
-module.exports = {
-  Contact,
-};
+module.exports = { Contact, schemas };
